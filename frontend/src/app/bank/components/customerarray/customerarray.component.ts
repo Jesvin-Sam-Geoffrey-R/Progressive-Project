d0036330891ad8c487402e76601907ed613baeb2
username <wecp-dummy@gmail.com>
2024-07-26 12:50:26 +0000

    wecp initial commit

--- frontend/src/app/bank/components/customerarray/customerarray.component.ts
+++ frontend/src/app/bank/components/customerarray/customerarray.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { of } from 'rxjs';
-import { Customer } from '../../types/Customer';
+import { CustomerTS } from '../../types/tstypes/Customerts';
 
 @Component({
   selector: 'app-customerarray',
-  standalone: true,
-  imports: [],
+  // standalone: true,
+  // imports: [],
   templateUrl: './customerarray.component.html',
-  styleUrl: './customerarray.component.css'
+  styleUrls: ['./customerarray.component.css']
 })
 export class CustomerarrayComponent {
-  
+  customers:CustomerTS[]=[
+    new CustomerTS('1','sam@gmail.com','sam','sam1','IT','2')
+  ];
 }
